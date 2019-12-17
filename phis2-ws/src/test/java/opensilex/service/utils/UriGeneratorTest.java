dc5d7569a104d42f53ff0a97cf2cff168fd79411
renaud colin <renaud.colin@inra.fr>
2019-12-17 16:18:51 +0100

    add Annotation and Instant generation unit tests
    Signed-off-by: renaud colin <renaud.colin@inra.fr>

--- phis2-ws/src/test/java/opensilex/service/utils/UriGeneratorTest.java
+++ phis2-ws/src/test/java/opensilex/service/utils/UriGeneratorTest.java
@@ -8,12 +8,15 @@
 
 package opensilex.service.utils;
 
+import opensilex.service.dao.AnnotationDAO;
 import opensilex.service.dao.EventDAO;
 import opensilex.service.dao.RadiometricTargetDAO;
 import opensilex.service.dao.UserDAO;
 import opensilex.service.dao.manager.Rdf4jDAOTest;
 import opensilex.service.model.*;
+import opensilex.service.ontology.Oa;
 import opensilex.service.ontology.Oeev;
+import opensilex.service.ontology.Time;
 import org.apache.commons.lang3.StringUtils;
 import org.joda.time.DateTime;
 import org.junit.jupiter.api.BeforeAll;
@@ -99,6 +102,48 @@ class UriGeneratorTest extends Rdf4jDAOTest {
 
         Set<String> uris = events.stream().map(RdfResourceDefinition::getUri).collect(Collectors.toSet());
         checkUris(uris,events);
+    }
+
+    @Test
+    public void testAnnotationGenerate() throws Exception{
+
+        Project testProject = createAndGetProject();
+        Experiment xp = createAndGetExperiment(testProject);
+        ScientificObject so = createAndGetScientificObject(xp);
+        Event event = createAndGetEvent(so.getUri());
+
+        int nbAnnotation = 5;
+        List<Annotation> annotations = new ArrayList<>(nbAnnotation);
+
+        ArrayList<String> bodyValues = new ArrayList<>();
+        bodyValues.add("annotate an event");
+        ArrayList<String> annotationTargetList = new ArrayList<>();
+        annotationTargetList.add(event.getUri());
+
+        for (int i = 0; i < nbAnnotation; i++) {
+            Annotation annotation =  new Annotation(null,DateTime.now(),userUri,bodyValues, Oa.INSTANCE_DESCRIBING.toString(),annotationTargetList);
+            annotations.add(annotation);
+        }
+        AnnotationDAO annotationDAO = new AnnotationDAO(userDao.findById(userUri));
+        annotationDAO.create(annotations);
+
+        Set<String> uris = annotations.stream().map(Annotation::getUri).collect(Collectors.toSet());
+        checkUris(uris,annotations);
+    }
+
+    @Test
+    public void testInstantGenerate() throws Exception{
+
+        int nbInstant = 5;
+
+        Set<String> uris = new HashSet<>();
+        for (int i = 0; i < nbInstant; i++) {
+            String instantUri = UriGenerator.generateNewInstanceUri(Time.Instant.toString(), null, null);
+            uris.add(instantUri);
+        }
+
+        // if the size are not equals, then it's means that there exist one duplicate URI, which is a fail
+        assertEquals(uris.size(),nbInstant);
     }
 
 }
