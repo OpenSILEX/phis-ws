276d4b7b1066fb65e6fb3dec297f1ec121f77760
ASA-OS-contrib2 <60963195+ASA-OS-contrib2@users.noreply.github.com>
2020-02-28 17:35:05 +0100

    ITests should pass

--- phis2-ws/src/test/java/opensilex/service/itests/OpenSilexWSITest.java
+++ phis2-ws/src/test/java/opensilex/service/itests/OpenSilexWSITest.java
@@ -179,7 +179,7 @@ public class OpenSilexWSITest extends InternalProviderIntegrationTestHelper {
         this.getToken();
         System.out.println("-----------Token: " + this.TOKKEN);
 
-        assertTrue("Token must be setted", this.TOKKEN.equals("")); // ITests should fail here
+        assertFalse("Token must be setted", this.TOKKEN.equals(""));
 
         postTestCaseTrace(nameofCurrMethod);
 
